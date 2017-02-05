0ad16da78f25a2dc0d2a04cfc062958a04a679a2
agostinor <agostino.romanelli@gmail.com>
2017-02-05 13:08:39 +0100

    Esercizio 2

---
+++ Esercizi 23:01:2017/Esercizio2/Esercizio2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Esercizio2
+//
+//  Created by Agostino Romanelli on 05/02/17.
+//  Copyright © 2017 AgostinoRomanelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Esercizi 23:01:2017/Esercizio2/Esercizio2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Esercizio2
+//
+//  Created by Agostino Romanelli on 05/02/17.
+//  Copyright © 2017 AgostinoRomanelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
